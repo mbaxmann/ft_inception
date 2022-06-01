b57cb1193d67fef63c325c462fb31e909fd5e870
mbaxmann <mathias.bamann4@gmail.com>
2022-06-01 22:49:04 +0200

    ..

---
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User=' ';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
+
+CREATE DATABASE $WP_DATABASE_NAME;
+CREATE USER 'WP_DATABASE_USR'@'%' IDENTIFIED by '$WP_DATABASE_PWD';
+GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USR'@'%';
+FLUSH PRIVILEGES;
+
+exec /usr/bin/mysql --user=mysql --console
