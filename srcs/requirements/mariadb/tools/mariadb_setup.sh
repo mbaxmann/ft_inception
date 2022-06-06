aac42ebe41465e25f6ec41f35eb76d596666a69c
mbaxmann <mathias.bamann4@gmail.com>
2022-06-06 18:42:37 +0200

    ..

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM mysql.user WHERE User=' ';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
+service mysql start
 
-CREATE DATABASE $WP_DATABASE_NAME;
-CREATE USER 'WP_DATABASE_USR'@'%' IDENTIFIED by '$WP_DATABASE_PWD';
-GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USR'@'%';
-FLUSH PRIVILEGES;
+mysql -u root -e "CREATE DATABASE ${MARIADB_DATABASE};"
+mysql -u root -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -u root -e "GRANT PRIVILEGES ALL ON ${MARIADB_DATA_BASE}.* TO '${MARIADB_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
-exec /usr/bin/mysql --user=mysql --console
+service mysql stop
